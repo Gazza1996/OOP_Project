25e06f74deb033ea879e4b6f6a60c6518e9636fa
Gary Mannion <gkmannion@hotmail.com>
2017-01-08 14:02:09 +0000

    Add files via upload

--- src/gmit/sw/server/ServerRunner.java
+++ src/gmit/sw/server/ServerRunner.java
@@ -11,17 +11,5 @@ public class ServerRunner {
 		ServerSocket myServerSocket = new ServerSocket(7777, 100 , 
 				InetAddress.getByName ("127.0.0.1") );
 		
-<<<<<<< HEAD
 	}
-=======
-/*		int id = 0;
-		while (true)
-		{
-			Socket clientSocket = m_ServerSocket.accept();
-			ClientServiceThread cliThread = new ClientServiceThread(clientSocket, id++);
-			cliThread.start();
-*/
-/*		}//end while 
-*/	}
->>>>>>> 343bb2637adebdb820f50bf35f682d9df5238d53
 }//end main file
