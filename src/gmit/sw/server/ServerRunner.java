9590a3c4e43d75d1a9abdf0e2851d492a8ef65aa
Gazza1996 <gkmannion@hotmail.com>
2017-01-08 13:00:13 +0000

    Server

---
+++ src/gmit/sw/server/ServerRunner.java
@@ -0,0 +1,23 @@
+package gmit.sw.server;
+
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerRunner {
+	public static void main(String[] args) throws Exception {
+	
+		// creates the server location address
+		ServerSocket myServerSocket = new ServerSocket(7777, 100 , 
+				InetAddress.getByName ("127.0.0.1") );
+		
+/*		int id = 0;
+		while (true)
+		{
+			Socket clientSocket = m_ServerSocket.accept();
+			ClientServiceThread cliThread = new ClientServiceThread(clientSocket, id++);
+			cliThread.start();
+*/
+/*		}//end while 
+*/	}
+}//end main file
